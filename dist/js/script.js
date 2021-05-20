4d47caae7bcf4666ec9faafeb0ce6d1461f55eeb
sergfrol33 <sergfrol33@gmail.com>
2021-05-20 12:41:53 +0300

    form preventDefault

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,28 +93,29 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var myMap; // Дождёмся загрузки API и готовности DOM.
-
-ymaps.ready(init);
-
-function init() {
-  // Создание экземпляра карты и его привязка к контейнеру с
-  // заданным id ("map").
-  myMap = new ymaps.Map('map', {
-    // При инициализации карты обязательно нужно указать
-    // её центр и коэффициент масштабирования.
-    center: [55.76, 37.64],
-    // Москва
-    zoom: 10
-  }, {
-    searchControlProvider: 'yandex#search'
+window.addEventListener('DOMContentLoaded', function () {
+  var submitButton = document.querySelector('.contacts__submit');
+  var form = document.querySelector('form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
   });
 
-  document.getElementById('destroyButton').onclick = function () {
-    // Для уничтожения используется метод destroy.
-    myMap.destroy();
+  var initMap = function initMap() {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    new ymaps.Map('map', {
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [55.76, 37.64],
+      // Москва
+      zoom: 10
+    }, {
+      searchControlProvider: 'yandex#search'
+    });
   };
-}
+
+  ymaps.ready(initMap);
+});
 
 /***/ }),
 
--- src/js/block/contacts.js
+++ src/js/block/contacts.js
@@ -1,23 +1,21 @@
-var myMap;
+window.addEventListener('DOMContentLoaded',()=>{
+    const submitButton = document.querySelector('.contacts__submit')
+    const form = document.querySelector('form')
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+    })
 
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
-
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [55.76, 37.64], // Москва
-        zoom: 10
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
-
-    document.getElementById('destroyButton').onclick = function () {
-        // Для уничтожения используется метод destroy.
-        myMap.destroy();
-    };
-
-}
+    const initMap = () => {
+        // Создание экземпляра карты и его привязка к контейнеру с
+        // заданным id ("map").
+        new ymaps.Map('map', {
+            // При инициализации карты обязательно нужно указать
+            // её центр и коэффициент масштабирования.
+            center: [55.76, 37.64], // Москва
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+    }
+    ymaps.ready(initMap);
+})
